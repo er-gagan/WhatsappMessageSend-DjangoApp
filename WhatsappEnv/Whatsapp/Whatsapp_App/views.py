786417976a27bb636345d3fd67dfd93c3276194a
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-07-15 16:34:36 +0530

    Value get frontend to backend
    Value get frontend to backend

--- WhatsappEnv/Whatsapp/Whatsapp_App/views.py
+++ WhatsappEnv/Whatsapp/Whatsapp_App/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request,"Whatsapp/home.html")
+
+def SendData(request):
+    if request.method == 'POST':
+        Num = request.POST['Phone']
+        Msg = request.POST['Message']
+        print(Num,Msg)
+        info = "Message has successfully sent.."
+        return render(request,"Whatsapp/home.html",{'info':info})
+    else:
+        return HttpResponse("<h1>404 - Not Found :(</h1>")
